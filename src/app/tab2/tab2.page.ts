810fc22b1193a31af1b2f5315906a9ac20128db2
LucasZubu <lucascaires3641@gmail.com>
2022-10-03 20:41:29 -0300

    Subindo

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,47 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  operacao = '';
+  resultado = '';
+  mat = '';
+
   constructor() {}
+
+  ngOninit() {}
+
+  adicionarValor(valor: any){
+    this.operacao += valor;
+  }
+
+  adicionarMatematica(matematica: any){
+    this.mat += matematica;
+    if (this.operacao != '') {
+      if ( this.mat.length == 1 ){
+        this.operacao += this.mat;
+      }
+    }
+  }
+
+  limparMemoria(){
+    this.operacao ='';
+    this.resultado ='';
+    this.mat = ''; 
+  }
+
+  limparOperacao(){
+    this.operacao ='';
+    this.mat = ''
+  }
+
+  iverterValor(){
+    //Alterna
+  }
+
+  apagarCaracter(){
+    if (this.operacao.length > 0) {
+    this.operacao = this.operacao.substring(0, this.operacao.length -1);
+    }
+  }
+
 
 }
