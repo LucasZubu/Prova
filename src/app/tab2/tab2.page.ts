2881feda76ca604351fe1454c28c58a33dcfd5cc
LucasZubu <lucascaires3641@gmail.com>
2022-10-17 20:31:47 -0300

    subindo

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { evaluate } from 'mathjs';
 
 @Component({
   selector: 'app-tab2',
@@ -10,20 +11,36 @@ export class Tab2Page {
   operacao = '';
   resultado = '';
   mat = '';
+  ultimoverica = false;
+  ultimo = false;
 
   constructor() {}
 
   ngOninit() {}
 
+  realizaOperacao(){
+    try{
+    this.operacao = evaluate(this.operacao);
+    }catch(err){
+      this.resultado = 'Inválido!';
+    }
+  }
+
   adicionarValor(valor: any){
     this.operacao += valor;
+    this.ultimo = false;
+
   }
 
   adicionarMatematica(matematica: any){
     this.mat += matematica;
-    if (this.operacao != '') {
-      if ( this.mat.length == 1 ){
+    if (this.operacao.length > 0) {
+      
+      if ( this.ultimo == false  && this.ultimoverica == false)
+      {
         this.operacao += this.mat;
+        this.ultimo = true;
+        this.ultimoverica = true;
       }
     }
   }
@@ -32,11 +49,15 @@ export class Tab2Page {
     this.operacao ='';
     this.resultado ='';
     this.mat = ''; 
+    this.ultimo = false;
+    this.ultimoverica = false;
   }
 
   limparOperacao(){
     this.operacao ='';
-    this.mat = ''
+    this.mat = '';
+    this.ultimo = false;
+    this.ultimoverica = false;
   }
 
   iverterValor(){
@@ -46,6 +67,13 @@ export class Tab2Page {
   apagarCaracter(){
     if (this.operacao.length > 0) {
     this.operacao = this.operacao.substring(0, this.operacao.length -1);
+
+      if (this.ultimoverica == true)
+      {
+        this.mat = '';
+        this.ultimo = false;
+        this.ultimoverica = false;
+      }
     }
   }
 
